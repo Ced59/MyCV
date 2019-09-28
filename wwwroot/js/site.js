bba7d8c804833e7a8f1bd26f5caa998d6f71e1a6
Cedric Caudron <c.caudron59@gmail.com>
2019-09-28 23:07:52 +0200

    Add Calcul Age in JQuery

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,30 @@
 ﻿
 $(function () {
-    // Sidebar toggle behavior
+    // Masquer et démasquer le menu
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar, #content').toggleClass('active');
     });
+});
+
+
+$(function () {
+    //Calcul de l'age
+    var today_date = new Date();
+    var today_year = today_date.getFullYear();
+    var today_month = today_date.getMonth();
+    var today_day = today_date.getDate();
+    var birth_year = '1984';
+    var birth_month = '10';
+    var birth_day = '2';
+    var age = today_year - birth_year;
+
+    if (today_month < (birth_month - 1)) {
+        age--;
+    }
+    if (((birth_month - 1) == today_month) && (today_day < birth_day)) {
+        age--;
+    }
+
+    $("#age").html('<p class="lead text-white mb-0">' + age + ' ans</p>');
+
 });
